c6556ebebd4c5b60c3dcb3fa108f0c12a59b7f65
MattiaSalve <mattiasalvetti@Mattias-MacBook-Air.local>
2024-05-07 13:55:38 +0200

    Added autoformatting

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,78 +1,80 @@
-return{
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'saadparwaiz1/cmp_luasnip'},
-	{'hrsh7th/nvim-cmp',
-    event = {"InsertEnter", "CmdlineEnter"},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+return {
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    {
+        'hrsh7th/nvim-cmp',
+        event = { "InsertEnter", "CmdlineEnter" },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
 
-		vim.opt.completeopt = "menu,menuone,noselect"
+            vim.opt.completeopt = "menu,menuone,noselect"
 
-        local lsp_zero = require("lsp-zero")
+            local lsp_zero = require("lsp-zero")
 
-        lsp_zero.on_attach(function(bufnr)
-            lsp_zero.default_keymaps({buffer = bufnr})
-        end)
+            lsp_zero.on_attach(function(bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
+            end)
 
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
-            ensure_installed = {},
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-            },
-        })
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = {},
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({
+                        })
+                    end,
+                },
+            })
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-                    luasnip.lsp_expand(args.body)
-				end,
-			},
-			sources = cmp.config.sources{
-				{ name = "nvim_lsp"},
-				{ name = "luasnip"},
-				{ name = "codeium"},
-				{ name = "path"},
-				{ name = "buffer"},
-				{ name = "cmp_r"},
-			},
-			mapping = {
-				['<Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, {"i", "s"}),
-				['<S-Tab>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.expand_or_jumpable(-1) then
-						luasnip.expand_or_jump(-1)
-					else
-						fallback()
-					end
-				end, {"i", "s"}),
-				['<CR>'] = cmp.mapping.confirm({
-                    select = true,
-                    behavior = cmp.ConfirmBehavior.Replace,
-                }),
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                sources = cmp.config.sources {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "codeium" },
+                    { name = "path" },
+                    { name = "buffer" },
+                    { name = "cmp_r" },
+                },
+                mapping = {
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.expand_or_jumpable(-1) then
+                            luasnip.expand_or_jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ['<CR>'] = cmp.mapping.confirm({
+                        select = true,
+                        behavior = cmp.ConfirmBehavior.Replace,
+                    }),
 
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			}
-		})
-	end,
-},
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                }
+            })
+        end,
+    },
 }
--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,38 +1,28 @@
-return{
-    'mhartington/formatter.nvim',
-    config = function ()
-        local util = require "formatter.util"
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-        require("formatter").setup {
-            logging = true,
-            log_level = vim.log.levels.WARN,
-            filetype = {
-                lua = {
-                    require("formatter.filetypes.lua").ast_grep,
-                    function()
-                        if util.get_current_buffer_file_name() == "special.lua" then
-                            return nil
-                        end
-                        return {
-                            exe = "ast_grep",
-                            args = {
-                                "--search-parent-directories",
-                                "--stdin-filepath",
-                                util.escape_path(util.get_current_buffer_file_path()),
-                                "--",
-                                "-",
-                            },
-                            stdin = true,
-                        }
-                    end
-                },
-                tex = {
-                    require("formatter.filetypes.tex").latexindent
-                },
-                ["*"] = {
-                    require("formatter.filetypes.any").remove_trailing_whitespace,
-                }
-            }
-        }
-    end
+		conform.setup({
+			formatters_by_ft = {
+				python = { "black" },
+				lua = { "stylua" },
+				latex = { "latexindent" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set("n, v", "<leader>fo", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
